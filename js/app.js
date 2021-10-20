7e8937cb6e0406de44097131a80b8b09e7e5633e
Marwan Ghanem <90506494+Marwann11@users.noreply.github.com>
2021-10-21 01:46:14 +0200

    feat: Change ul

---
+++ js/app.js
@@ -0,0 +1,86 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+// declare all sections in a variable 
+const mySections = document.querySelectorAll('[data-nav]');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+function sectionNum() {
+
+}
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+// declare the ul
+const navBar = document.querySelector('#navbar__list');
+
+// iterate over mySections variable and add li dynamically
+mySections.forEach(section => {
+  
+  // create Element
+  const listItem = document.createElement('li');
+
+  // assign text to each listItem
+  listItem.textContent = section.getAttribute('data-nav');
+  // assign it to variable
+  const listText = listItem.textContent;
+
+  // add class for each listItem
+  listItem.className = 'list__edit'
+
+  // add anchor to section
+  listItem.innerHTML = `<a href="#${section.id}">${listText}</a>`;
+
+  // add smooth scroll
+  section.id.scrollIntoView({behavior: "smooth",block: 'start',inline: 'nearest'})
+
+  // append to navBar
+  navBar.appendChild(listItem);
+
+});
+
+
+// Add class 'active' to section when near top of viewport
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
